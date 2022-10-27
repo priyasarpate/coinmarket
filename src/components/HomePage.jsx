13184387d9e75a81f056588656e014c73830603e
Gunpriya Sarpate <sarpatepriya@gmail.com>
2022-10-27 12:45:25 +0530

    added cards on homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
+import { Card } from "antd";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 function HomePage() {
-
-  const {data, isFetching} = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery();
 
   console.log(data);
   return (
@@ -16,22 +16,53 @@ function HomePage() {
       <Title level={2} className="heading">
         Global Crypto Stat
       </Title>
+
       <Row>
-        <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
+        <Col span={6}>
+          <Card
+            style={{
+              width: 200,
+            }}
+          >
+            <Statistic title="Total Cryptocurrencies" value="5" />
+          </Card>
         </Col>
-        <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+        <Col span={6}>
+          <Card
+            style={{
+              width: 200,
+            }}
+          >
+            <Statistic title="Total Exchanges" value="5" />
+          </Card>
         </Col>
-        <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
+        <Col span={6}>
+          <Card
+            style={{
+              width: 200,
+            }}
+          >
+            <Statistic title="Total Market Cap" value="5" />
+          </Card>
         </Col>
-        <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+        <Col span={6}>
+          <Card
+            style={{
+              width: 200,
+            }}
+          >
+            <Statistic title="Total 24h Volume" value="5" />
+          </Card>
         </Col>
-        <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
-        </Col>
+        {/* <Col span={6}>
+          <Card
+            style={{
+              width: 200,
+            }}
+          >
+            <Statistic title="Total Cryptocurrencies" value="5" />
+          </Card>
+        </Col> */}
       </Row>
     </>
   );
